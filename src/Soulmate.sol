c3ac29030c8d215d319a2d14b995ac9d38b42286
n0kto <you@example.com>
2024-02-05 15:48:23 +0100

    added protocol contracts

--- src/Soulmate.sol
+++ src/Soulmate.sol
@@ -1,81 +1,142 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 import {ERC721} from "@solmate/tokens/ERC721.sol";
 
+/// @title Soulmate Soulbound NFT.
+/// @author n0kto
+/// @notice A Soulbound NFT sharing by soulmates.
 contract Soulmate is ERC721 {
-    uint256 nextID = 1;
+    /*//////////////////////////////////////////////////////////////
+                                ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error Soulmate__alreadyHaveASoulmate(address soulmate);
+    error Soulmate__SoulboundTokenCannotBeTransfered();
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
 
     string[4] niceWords = ["sweetheart", "darling", "my dear", "honey"];
 
-    mapping(uint256 id => address[2] owners) public idToOwners;
+    mapping(uint256 id => address[2] owners) private idToOwners;
+    mapping(uint256 id => uint256 timestamp) public idToCreationTimestamp;
+    mapping(address soulmate1 => address soulmate2) public soulmateOf;
     mapping(address owner => uint256 id) public ownerToId;
 
-    mapping(bytes32 id => string) public sharedSpace;
+    mapping(address owner => bool isDivorced) private divorced;
 
-    modifier alreadyHaveASoulmate() {
-        require(ownerToId[msg.sender] == 0);
-        _;
-    }
+    mapping(uint256 id => string) public sharedSpace;
+
+    uint256 private nextID;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event MessageWrittenInSharedSpace(uint256 indexed id, string message);
+
+    event SoulmateIsWaiting(address indexed soulmate);
+
+    event SoulmateAreReunited(
+        address indexed soulmate1,
+        address indexed soulmate2,
+        uint256 indexed tokenId
+    );
+
+    event CoupleHasDivorced(
+        address indexed soulmate1,
+        address indexed soulmate2
+    );
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     constructor() ERC721("Soulmate", "SLMT") {}
 
-    function mintToken() public alreadyHaveASoulmate {
-        // Since there is only 8 billions people on earth, there is no need to check the supply.
-        // known issue : no limit mint
-        if (idToOwners[nextID][0] == address(0)) {
+    /// @notice Used to mint a token when soulmates are reunited.
+    /// @notice Soulmates are reunited every time a second people try to mint the same ID.
+    /// @return ID of the minted NFT.
+    function mintSoulmateToken() public returns (uint256) {
+        // Check if people already have a soulmate, which means already have a token
+        address soulmate = soulmateOf[msg.sender];
+        if (soulmate != address(0))
+            revert Soulmate__alreadyHaveASoulmate(soulmate);
+
+        address soulmate1 = idToOwners[nextID][0];
+        address soulmate2 = idToOwners[nextID][1];
+        if (soulmate1 == address(0)) {
             idToOwners[nextID][0] = msg.sender;
             ownerToId[msg.sender] = nextID;
-        } else if (idToOwners[nextID][1] == address(0)) {
+            emit SoulmateIsWaiting(msg.sender);
+        } else if (soulmate2 == address(0)) {
             idToOwners[nextID][1] = msg.sender;
             // Once 2 soulmates are reunited, the token is minted
+            ownerToId[msg.sender] = nextID;
+            soulmateOf[msg.sender] = soulmate1;
+            soulmateOf[soulmate1] = msg.sender;
+            idToCreationTimestamp[nextID] = block.timestamp;
+
+            emit SoulmateAreReunited(soulmate1, soulmate2, nextID);
+
             _mint(msg.sender, nextID++);
         }
-        ownerToId[msg.sender] = nextID;
+
+        return ownerToId[msg.sender];
     }
 
+    /// @dev will be added after audit.
+    /// @dev Since it is only used by wallets, it won't create any edge case.
     function tokenURI(uint256) public pure override returns (string memory) {
+        // To do
         return "";
     }
 
-    function totalSupply() external view returns (uint256) {
-        return nextID - 1;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 id
-    ) public override {
+    /// @notice Override of transferFrom to prevent any transfer.
+    function transferFrom(address, address, uint256) public pure override {
         // Soulbound token cannot be transfered
-
-        // This require is enough thanks to those in the parent function: from == msg.sender and from == _owner(id)
-        require(ownerToId[msg.sender] != id);
-        super.transferFrom(from, to, id);
+        // Having a soulmate is for life !
+        revert Soulmate__SoulboundTokenCannotBeTransfered();
     }
 
-    function writeMessageInSharedSpace(
-        address soulmate1,
-        address soulmate2,
-        string calldata message
-    ) external {
-        require(soulmate1 == msg.sender || soulmate2 == msg.sender);
-        bytes32 id = keccak256(abi.encodePacked(soulmate1, soulmate2));
+    /// @notice Allows any soulmates with the same NFT ID to write in a shared space on blockchain.
+    /// @param message The message to write in the shared space.
+    function writeMessageInSharedSpace(string calldata message) external {
+        uint256 id = ownerToId[msg.sender];
         sharedSpace[id] = message;
+        emit MessageWrittenInSharedSpace(id, message);
     }
 
-    function readMessageInSharedSpace(
-        address soulmate1,
-        address soulmate2
-    ) external view returns (string memory) {
-        bytes32 id = keccak256(abi.encodePacked(soulmate1, soulmate2));
-
+    /// @notice Allows any soulmates with the same NFT ID to read in a shared space on blockchain.
+    function readMessageInSharedSpace() external view returns (string memory) {
         // Add a little touch of romantism
         return
             string.concat(
-                sharedSpace[id],
+                sharedSpace[ownerToId[msg.sender]],
                 ", ",
                 niceWords[block.timestamp % niceWords.length]
             );
+    }
+
+    /// @notice Cancel possibily for 2 lovers to collect LoveToken from the airdrop.
+    function getDivorced() public {
+        address soulmate2 = soulmateOf[msg.sender];
+        divorced[msg.sender] = true;
+        divorced[soulmateOf[msg.sender]] = true;
+        emit CoupleHasDivorced(msg.sender, soulmate2);
+    }
+
+    function isDivorced() public view returns (bool) {
+        return divorced[msg.sender];
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return nextID;
+    }
+
+    function totalSouls() external view returns (uint256) {
+        return nextID * 2;
     }
 }
